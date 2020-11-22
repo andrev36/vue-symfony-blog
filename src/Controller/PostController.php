a670b732e1099b45e6e4810c6917513fb7733407
andrev36 <wiadernyandrzej96@gmail.com>
2020-11-22 16:42:45 +0100

    📦 NEW: User Class with security, showing blog

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -16,7 +16,7 @@ class PostController extends AbstractController
  public function createPost(): Response
  {
   // you can fetch the EntityManager via $this->getDoctrine()
-  // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+  // or you can add an argument to the action: createPost(EntityManagerInterface $entityManager)
   $entityManager = $this->getDoctrine()->getManager();
 
   $post = new Post();
@@ -26,12 +26,34 @@ class PostController extends AbstractController
   $post->setComment('First blog post comment');
   $post->setTimestamp(new \DateTime("now"));
 
-  // tell Doctrine you want to (eventually) save the Product (no queries yet)
+  // tell Doctrine you want to (eventually) save the Post (no queries yet)
   $entityManager->persist($post);
 
   // actually executes the queries (i.e. the INSERT query)
   $entityManager->flush();
 
   return new Response('Saved new product with id ' . $post->getId());
+ }
+ /**
+  * @Route("/post/{id}", name="post_show")
+  */
+ public function show(int $id): Response
+ {
+  $post = $this->getDoctrine()
+   ->getRepository(Post::class)
+   ->find($id);
+
+  if (!$post) {
+   throw $this->createNotFoundException(
+    'No post found for id ' . $id
+   );
+  }
+
+  return new Response('Check out this great product: ' . $post->getTitle());
+
+  // or render a template
+  // in the template, print things with {{ product.name }}
+  // return $this->render('product/show.html.twig',
+  // ['product' => $product]);
  }
 }
